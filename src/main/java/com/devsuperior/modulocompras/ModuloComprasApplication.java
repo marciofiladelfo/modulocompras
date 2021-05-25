446b3e74220aa55076951f717ada92927872557b
marciofiladelfo <marcio.kabana@gmail.com>
2021-05-24 23:29:51 -0300

    Projeto criado

---
+++ src/main/java/com/devsuperior/modulocompras/ModuloComprasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.modulocompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloComprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/modulocompras/ModuloComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.modulocompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
